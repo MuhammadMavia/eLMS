2081583aeab09c2020629dcb52ca55afbaa40965
muhammad mavia <muhammadmavia172@gmail.com>
2016-04-06 19:25:32 +0500

    youtube

--- server/index.js
+++ server/index.js
@@ -16,10 +16,11 @@ var lessons = require('./routes/lessons');
 app.use("/account", account);
 app.use("/courses", courses);
 app.use("/lessons", lessons);
-app.use(express.static(path.join(__dirname,'../public')));
+app.use(express.static(path.join(__dirname,'../youtube')));
+// app.use(express.static(path.join(__dirname,'../public')));
 
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
 });
